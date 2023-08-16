efab8128e14c80accf2d5af02eba467090c1e7e5
Kirill Streltsov <kirill.streltsov@7p-group.com>
2023-08-16 19:40:26 +0200

    Initial Commit

---
+++ SwiftUI_Concurrency/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI_Concurrency
+//
+//  Created by Streltsov, Kirill, SEVEN PRINCIPLES on 16.08.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Concurrency/SwiftUI_ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ConcurrencyApp.swift
+//  SwiftUI_Concurrency
+//
+//  Created by Streltsov, Kirill, SEVEN PRINCIPLES on 16.08.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
